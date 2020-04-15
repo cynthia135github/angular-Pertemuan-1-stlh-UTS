9c2d9de7752df8091a0159e56caed736dcc664f6
cynthia135github <c14180135@john.petra.ac.id>
2020-04-15 08:49:49 +0700

    2 files modified -  Array dan Tampilkan Array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Cynthia';
+
+  itembuah = [{
+    nama : 'kelengkeng',
+    harga : 10000
+
+  },{
+    nama:'jeruk',
+    harga : 12000
+  }];
 }
